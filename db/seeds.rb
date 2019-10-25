c80565281a7d901f590c03b6f78e910387f4ff87
BSarahB <bslajta@gmail.com>
2019-10-25 03:58:46 +0200

    adding files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,39 @@
+require 'faker'
+
+Dog.destroy_all
+Dogsitter.destroy_all
+Stroll.destroy_all
+City.destroy_all
+
+20.times do
+  Dog.create(nick_name: Faker::Name.first_name, city: City.all.sample)
+end
+
+20.times do
+  DogSitter.create(name: Faker::Name.first_name, city: City.all.sample)
+end
+
+20.times do
+  Stroll.create()
+end
+
+20.times do
+  City.create(name: Faker::Address.city)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
